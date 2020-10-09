806c95c75234839918800714a07f0d95aaedd9a4
Steven <bongamke@sheridancollege.ca>
2020-10-09 09:37:08 -0400

    Add files via upload

---
+++ HomeController.java
@@ -0,0 +1,30 @@
+package ca.sheridancollege.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import ca.sheridancollege.models.LocationStats;
+import ca.sheridancollege.services.CoronaVirusDataService;
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	CoronaVirusDataService coronaVirusDataService;
+
+	@GetMapping("/")
+	public String home(Model model) {
+		List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+		int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+		int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPreviousDay()).sum();
+		model.addAttribute("locationStats", allStats);
+		model.addAttribute("totalReportedCases", totalReportedCases);
+		model.addAttribute("totalNewCases", totalNewCases);
+		
+		return "home";
+	}
+}
